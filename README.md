5c98f39db7ba2e9b66d917ec403dbe5d8e5979ff
RabadonDex <lovallenunez@gmail.com>
2025-08-20 15:46:00 -0400

    weaitas

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-ola 
+Tomen esto como una bitacora de cosas que hago en la clase :D
--- app/page.js
+++ app/page.js
@@ -1,3 +1,14 @@
+import data from "./data/nombres.json"
+
 export default function Page() {
-  return <h1>funciona</h1>
+  return(
+  <>
+    <h1>{data.titulo}</h1>
+    <ul>
+      {data.palabras.map((palabras, idx)=>(
+        <li key={idx}>{palabras.nombre}</li>
+      ))}
+    </ul>
+  </>
+  );
 }
